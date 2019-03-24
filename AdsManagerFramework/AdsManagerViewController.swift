22c676af0519002211c5918fe4c5b3f8d3ab54c0
bfazeli <bfazeli1@gmail.com>
2019-03-24 09:49:52 -0700

    Update: Initial commit

---
+++ AdsManagerFramework/AdsManagerViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AdsManagerViewController.swift
+//  AdsManagerFramework
+//
+//  Created by Bijan Fazeli on 3/24/19.
+//  Copyright © 2019 Fazeli, Bijan. All rights reserved.
+//
+
+import UIKit
+
+public final class AdsManagerViewController: UIViewController {
+    
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
