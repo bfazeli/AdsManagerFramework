a2b5ca9058a19e3ca32d54df24c4a554403392cf
bfazeli <bfazeli1@gmail.com>
2019-03-24 09:20:02 -0700

    Initial Commit

---
+++ AdsManagerFramework/AdsManagerFramework.h
@@ -0,0 +1,19 @@
+//
+//  AdsManagerFramework.h
+//  AdsManagerFramework
+//
+//  Created by Bijan Fazeli on 3/24/19.
+//  Copyright © 2019 Fazeli, Bijan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AdsManagerFramework.
+FOUNDATION_EXPORT double AdsManagerFrameworkVersionNumber;
+
+//! Project version string for AdsManagerFramework.
+FOUNDATION_EXPORT const unsigned char AdsManagerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdsManagerFramework/PublicHeader.h>
+
+
